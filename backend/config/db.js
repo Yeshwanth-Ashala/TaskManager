28ff15f3616f92bb9145de5088cb11dca913d984
Yeshwanth Ashala <your_email@example.com>
2026-03-25 18:30:41 +0530

    fix: use environment variables for database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 
+
 const connectDB = async () => {
   try {
-    // We are hardcoding the link just to test!
-    const conn = await mongoose.connect('mongodb+srv://Yeshwanth:tasktracker123@tasktracker.w5w3nz2.mongodb.net/TaskManager?appName=TaskTracker');
+    // This tells it to look at Render's dashboard instead of a hardcoded string!
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.log(error);
     process.exit(1);
   }
 };
-
 export default connectDB;
